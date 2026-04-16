f13fd15e711c6a7001bb223d8e5599c42d9038b4
katie <katie.cullen25@mail.dcu.ie>
2026-04-16 19:31:30 +0100

    x

--- number.py
+++ number.py
@@ -31,3 +31,43 @@ here goes nothing
 
 
 xcvx
+
+ddddd
+
+is it when we both type at same timethis is a test file
+
+#this is a python file
+
+dont
+push cthat!
+#code
+
+hClas
+sdsd
+njb
+hhwe
+fjfjfyjyffdjyfyyfjyjpending-voice-note$0ffjy
+j
+hi?
+im good
+hellookkkpendzing-voice-note$0
+hi?
+
+hello
+how are ypending-voice-note$0ou
+okl
+doo
+hicpending-voice-note$0c
+hello?
+hh
+ok will this save
+lets try??
+
+here goes nothing
+
+
+xcvx
+
+ddddd
+
+is it when we both type at same time
